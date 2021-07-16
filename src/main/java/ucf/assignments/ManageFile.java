4cfa3f39b294ce8cabe76988032b9624a8aa6f5a
Monz <69062179+khyrdantai@users.noreply.github.com>
2021-07-16 13:54:27 -0400

    Update 1.

--- src/main/java/ucf/assignments/ManageFile.java
+++ src/main/java/ucf/assignments/ManageFile.java
@@ -78,5 +78,4 @@ public class ManageFile {
 		return null;
 	}
 
-
 }
--- src/main/java/ucf/assignments/Search.java
+++ src/main/java/ucf/assignments/Search.java
@@ -11,20 +11,19 @@ public class Search {
 
 	public void findItem(ObservableList<InventoryItem> origin, ObservableList<InventoryItem> destination, String string) {
 		// load in the origin
-		// for each item in the origin
+		// for each item in the origin, if it matches the serial or description, add it to the destination
 
 		for (InventoryItem item : origin) {
+			// if serial matches, add to destination and skip to next InventoryItem
 			if (string.equals(item.getItemSerial())) {
 				destination.add(item);
 				continue; // skips the next "if" so it's not added twice
 			}
-
+			// if description contains anything related, adds to destination and continues loop
 			if (item.getItemDescription().contains(string)) {
 				destination.add(item);
 			}
 
 		}
-
 	}
-
 }
