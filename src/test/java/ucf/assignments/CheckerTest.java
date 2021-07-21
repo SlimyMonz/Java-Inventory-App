ef47fecc324307588b2d20b816501c330de78b40
Monz <69062179+khyrdantai@users.noreply.github.com>
2021-07-21 19:32:08 -0400

    Update 7: Updated test files to conform to new code. Still a WIP for some.

---
+++ src/test/java/ucf/assignments/CheckerTest.java
@@ -0,0 +1,33 @@
+/*
+ * UCF COP3330 Summer 2021 Assignment Solution
+ * Copyright 2021 Harry Hocker
+ */
+
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckerTest {
+
+	@Test
+	void duplicates() {
+	}
+
+	@Test
+	void valueFormat() {
+	}
+
+	@Test
+	void serialLength() {
+	}
+
+	@Test
+	void validDescription() {
+	}
+
+	@Test
+	void allValues() {
+	}
+}
