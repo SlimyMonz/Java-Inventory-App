eb1e935c5061f7bbe5a4f5741004c46e17ac9960
Monz <69062179+khyrdantai@users.noreply.github.com>
2021-07-22 14:23:57 -0400

    Update 8: Updated test files to conform to new code. Still a WIP for some.

--- src/test/java/ucf/assignments/InventoryItemTest.java
+++ src/test/java/ucf/assignments/InventoryItemTest.java
@@ -11,31 +11,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InventoryItemTest {
 
-	String itemValue = "yeah";
+	String itemValue = "$100";
 	String itemSerial = "1111111111";
-	String itemName = "cool";
+	String itemName = "bingo";
 
 	@Test
 	void getItemValue() {
+		assertEquals("$100", itemValue);
 	}
 
 	@Test
 	void getItemSerial() {
+		assertEquals("1111111111", itemSerial);
 	}
 
 	@Test
 	void getItemName() {
+		assertEquals("bingo", itemName);
 	}
 
 	@Test
 	void setItemValue() {
+		itemValue = "$1";
+		assertEquals("$1", itemValue);
 	}
 
 	@Test
 	void setItemSerial() {
+		itemSerial = "2222222222";
+		assertEquals("2222222222", itemSerial);
 	}
 
 	@Test
 	void setItemName() {
+		itemName = "not bingo";
+		assertEquals("not bingo", itemName);
 	}
 }
