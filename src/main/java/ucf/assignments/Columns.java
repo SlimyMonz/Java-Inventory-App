8ced01361a8e959ad9b2b4515f928297061ccdcb
Harry Hocker <69062179+SlimyMonz@users.noreply.github.com>
2023-11-16 13:26:16 -0500

    Code cleanup.

--- src/main/java/ucf/assignments/Columns.java
+++ src/main/java/ucf/assignments/Columns.java
@@ -9,37 +9,20 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 
-
 public class Columns {
 
-	// REALLY IMPORTANT NOTE FOR GRADER: THIS IS JAVAFX STUFF! Cannot test it properly! There will NOT be a test class for this file!
+    public void setValueColumn(TableColumn<InventoryItem, String> valueColumn) {
+        valueColumn.setCellValueFactory(new PropertyValueFactory<>("itemValue"));
+        valueColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+    }
 
-	public void setValueColumn(TableColumn<InventoryItem, String> valueColumn) {
+    public void setSerialColumn(TableColumn<InventoryItem, String> serialColumn) {
+        serialColumn.setCellValueFactory(new PropertyValueFactory<>("itemSerial"));
+        serialColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+    }
 
-		// set each cell to a value from InventoryItem's object value string
-		valueColumn.setCellValueFactory(new PropertyValueFactory<>("itemValue"));
-		// set the cell as a text field for the entire column
-		valueColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-
-	}
-
-	public void setSerialColumn(TableColumn<InventoryItem, String> serialColumn) {
-
-		// set each cell to a value from InventoryItem's serial object string
-		serialColumn.setCellValueFactory(new PropertyValueFactory<>("itemSerial"));
-		// set the cell as a text field for the entire column
-		serialColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-
-	}
-
-	public void setNameColumn(TableColumn<InventoryItem, String> NameColumn) {
-
-		// set column to a text Name from string of object
-		NameColumn.setCellValueFactory(new PropertyValueFactory<>("itemName"));
-		// set the entire column as a text field
-		NameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-
-	}
-
-
+    public void setNameColumn(TableColumn<InventoryItem, String> NameColumn) {
+        NameColumn.setCellValueFactory(new PropertyValueFactory<>("itemName"));
+        NameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+    }
 }
